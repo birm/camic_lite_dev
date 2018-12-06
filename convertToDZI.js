4baca40e6be5cdcffc4bfd9e49473e975951997d
Ryan Birmingham <birm@rbirm.us>
2018-12-06 17:28:57 -0500

    add linux and python bins

--- convertToDZI.js
+++ convertToDZI.js
@@ -1,9 +1,8 @@
 var sharp = require('sharp');
 
-function convertToDZI(input, output){
-  sharp(input).tile(255).toFile(output, function(err, info) {
-    // The output.dzi file is the XML format Deep Zoom definition
-    // The output_files directory contains 256x256 pixel tiles grouped by zoom level
+function convertToDZI(input, output, callback){
+  return sharp(input).limitInputPixels(false).tile(255).toFile(output, function(err, info) {
+    callback(err, info)
   });
 }
 
